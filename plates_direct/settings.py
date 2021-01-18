9ba0df72cbee877d1403de22bfcd81172bc6ff71
alex <alex96ford19@gmail.com>
2021-01-18 23:31:19 +0000

    Added the faqs app and linked the app to the project. Also added important information about the companies services.

--- plates_direct/settings.py
+++ plates_direct/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'design_plate',
+    'faqs',
 ]
 
 MIDDLEWARE = [
--- plates_direct/urls.py
+++ plates_direct/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
     path("design_plates/", include("design_plate.urls")),
+    path("faqs/", include("faqs.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
